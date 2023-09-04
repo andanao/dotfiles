be9d06d177664171bd44f1a64e423162923fa16c
adriandanao <adriandanao@gmail.com>
2023-09-04 14:38:14 -0400

    laptoff and on

--- .config/aliases/arch.sh
+++ .config/aliases/arch.sh
@@ -7,3 +7,8 @@ alias yeet="paru -Rcs"
 
 alias nightlight="lux -S 1%; gammastep -O 2500 &"
 alias daylight="lux -S 100%; killall gammastep"
+
+alias laptoff="hyprctl keyword monitor eDP-1, disable"
+alias lapton="hyprctl keyword monitor eDP-1, enable"
+
+alias jkgs="cd $user_git_dir/hugo-site"
