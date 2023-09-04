02787e416c14ab1910685b50478aac6e51c2cae9
adriandanao <adriandanao@gmail.com>
2023-09-04 16:05:34 -0400

    fix lapton

--- .config/aliases/arch.sh
+++ .config/aliases/arch.sh
@@ -9,6 +9,6 @@ alias nightlight="lux -S 1%; gammastep -O 2500 &"
 alias daylight="lux -S 100%; killall gammastep"
 
 alias laptoff="hyprctl keyword monitor eDP-1, disable"
-alias lapton="hyprctl keyword monitor eDP-1, enable"
+alias lapton="hyprctl keyword monitor eDP-1, eDP-1,highres,auto,1"
 
 alias jkgs="cd $user_git_dir/hugo-site"
